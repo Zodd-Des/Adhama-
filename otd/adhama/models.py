fc43d89030622f044ea26d2d21bfbd2bb11f40a7
Dess <desmondotachi1@gmail.com>
2020-08-17 14:35:03 +0300

    first debugging ,minor changes on the template

--- otd/adhama/models.py
+++ otd/adhama/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Job:
+    id : int
+    name : str
+    img : str
+    desc : str
+
+class Testimony:
+    id : int
+    name : str
+    name2 : str
+    img : str
+    desc : str
+
+
+
+
