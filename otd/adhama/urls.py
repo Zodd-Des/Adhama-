0620387524955d0425ff28abdbd041d828560452
Dess <desmondotachi1@gmail.com>
2020-08-22 11:54:14 +0300

    adding the other templates

--- otd/adhama/urls.py
+++ otd/adhama/urls.py
@@ -2,4 +2,13 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path('index.html', views.index, name='index'),
+    path('about.html', views.about, name='about'),
+    path('offers.html', views.offers, name='offers'),
+    path('elements.html', views.elements, name='elements'),
+    path('blog.html', views.blog, name='blog'),
+    path('contact.html', views.contact, name='contact'),
+    path('privacy.html', views.privacy, name='privacy'),
+    path('terms.html', views.terms, name='terms')
+    ]
--- otd/adhama/views.py
+++ otd/adhama/views.py
@@ -12,4 +12,39 @@ def index(request):
 
 
     return render(request, "index.html",{'dests': dests, 'destis': destis})
+
+def about(request):
+
+    return render(request, "about.html")
+
+    
+def offers(request):
+
+    return render(request, "offers.html")
+    
+       
+def elements(request):
+
+    return render(request, "elements.html")
+    
+    
+def blog(request):
+
+    return render(request, "blog.html")
+    
+    
+def contact(request):
+
+    return render(request, "contact.html")
+    
+    
+def privacy(request):
+
+    return render(request, "privacy.html")
+    
+    
+def terms(request):
+
+    return render(request, "terms.html")
+    
     
